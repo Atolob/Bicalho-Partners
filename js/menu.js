2c80c3667c5dd2af0f323861dd816472fb514697
Guilherme Bicalho <bicalhoguilherme63@outlook.com>
2026-06-12 17:54:02 -0300

    Mudança celular e pc

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,18 @@
-// Espaço para futuras interações do menu / mega menu.
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", function () {
+  menu.classList.toggle("open");
+  hamburger.classList.toggle("open");
+
+  const estaAberto = menu.classList.contains("open");
+  hamburger.setAttribute("aria-expanded", estaAberto);
+});
+
+menu.querySelectorAll("a").forEach(function (link) {
+  link.addEventListener("click", function () {
+    menu.classList.remove("open");
+    hamburger.classList.remove("open");
+    hamburger.setAttribute("aria-expanded", false);
+  });
+});
